0e50afbbbd4edc97a42a6e4a7306d3fe225f2fbc
Ed Stephenson <edward.stephenson@me.com>
2021-02-21 15:23:44 +0000

    import statements

---
+++ src/channels/http/index.ts
@@ -0,0 +1,2 @@
+export * from './http-notification.interface';
+export * from './http.channel';
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './nestjs-notifications.module';
+export * from './notifications.service';
+export * from './notification/notification.interface';
+export * from './channels';
