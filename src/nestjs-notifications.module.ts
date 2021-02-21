559e9b57dbb1d2a3b48f84f4939119c61199121d
Ed Stephenson <edward.stephenson@me.com>
2021-02-21 14:05:48 +0000

    make module global

--- src/nestjs-notifications.module.ts
+++ src/nestjs-notifications.module.ts
@@ -1,8 +1,17 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { DynamicModule, HttpModule, Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
 
 @Module({
   imports: [HttpModule],
   providers: [NotificationsService],
 })
-export class NestJsNotificationsModule {}
+export class NestJsNotificationsModule {
+  static forRoot(): DynamicModule {
+    return {
+      global: true,
+      module: NestJsNotificationsModule,
+      providers: [NotificationsService],
+      exports: [NotificationsService],
+    };
+  }
+}
