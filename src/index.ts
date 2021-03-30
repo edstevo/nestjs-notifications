5057b8f498a5604d5b428668c436a471930195d8
Ed Stephenson <edward.stephenson@me.com>
2021-03-30 19:36:15 +0100

    fix for update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export * from './nestjs-notifications.module-as';
 export * from './nestjs-notifications.service';
 export * from './notification/notification.interface';
 export * from './channels';
