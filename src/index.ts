30df22940a2bdf7c8870dd76012df4e2d7c23fa8
Ed Stephenson <edward.stephenson@me.com>
2021-03-30 19:16:50 +0100

    Remove unnecessary module

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './nestjs-notifications.module';
+export * from './nestjs-notifications.module-as';
 export * from './nestjs-notifications.service';
 export * from './notification/notification.interface';
 export * from './channels';
